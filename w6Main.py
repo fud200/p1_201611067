5d5ae29e1860ca7851033778c310b9037eef2171
fud200 <fud1020@naver.com>
2016-04-06 14:58:23 +0900

    3과5의 배수
    3과 5의 배수 합

---
+++ w6Main.py
@@ -0,0 +1,12 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def multiple():
+	sum=0
+	for i in range (0,1000):
+		if not i%3 or i%5:
+			sum=sum+i
+	print sum
+multiple()
+
+wn.exitonclick()
