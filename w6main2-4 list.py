2fbfac23027092d883162e354588a259d7ae5b24
fud200 <fud1020@naver.com>
2016-04-11 15:20:14 +0900

    6주차 리스트과제
    6주차 리스트과제

---
+++ w6main2-4 list.py
@@ -0,0 +1,16 @@
+﻿h=list() 
+def sumList(h): 
+    	for i in range(0,1001): 
+		if(i%4==0 and i%5>0): 
+			h.append(i) 
+	sum=0 
+	for j in range(0,len(h)): 
+		sum=sum+h[j] 
+	return sum	 
+def main(): 
+	print sumList(h) 
+main() 
+
+input("enter to end")
+if __name__=="__main__": 
+	main() 
