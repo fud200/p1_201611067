ed824118e8045f48917f8c234885f0bd4636f802
fud200 <fud1020@naver.com>
2016-04-18 23:41:22 +0900

    7주차과제 dict
    7주차과제 dict

---
+++ w7main3.py
@@ -0,0 +1,17 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+d=dict()
+d=({0:(0,0),1:(70,0),2:(70,70),3:(0,70),4:(0,0)})
+def lab7_3():
+	for i in range (1,5):
+		t1.goto(d[i])
+	for h in range (1,5):
+		print d[h]
+	wn.exitonclick()
+
+def main():
+	lab7_3()
+
+if __name__=="__main__":
+	main()
