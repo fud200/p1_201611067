afdf2d4c1b10b288805fb03b19259768c12c7b39
fud200 <fud1020@naver.com>
2016-04-20 15:46:58 +0900

    7주차과제
    7주차과제

---
+++ w7main4.py
@@ -0,0 +1,65 @@
+﻿import turtle  
+wn=turtle.Screen()  
+t1=turtle.Turtle()  
+
+def saveTracks(): 
+    mytracks=list() 
+    mytracks.append(t1.pos()) 
+    t1.speed(2) 
+    t1.pencolor("Blue")  
+    t1.penup() 
+    mytracks=list() 
+    t1.goto(-400,300) 
+    mytracks.append(t1.pos()) 
+    t1.pendown()   
+    t1.right(90)  
+    t1.fd(400) 
+    mytracks.append(t1.pos()) 
+    t1.backward(150)  
+    mytracks.append(t1.pos()) 
+    t1.left(90)  
+    t1.fd(300)  
+    mytracks.append(t1.pos()) 
+    t1.left(90)  
+    t1.fd(300) 
+    mytracks.append(t1.pos()) 
+    t1.back(150)  
+    mytracks.append(t1.pos()) 
+    t1.right(90)  
+    t1.fd(300)  
+    mytracks.append(t1.pos()) 
+    t1.left(90)  
+    t1.right(90)  
+    t1.right(90)  
+    t1.fd(200)  
+    mytracks.append(t1.pos()) 
+    t1.fd(300)  
+    mytracks.append(t1.pos()) 
+    t1.back(100)  
+    mytracks.append(t1.pos()) 
+    t1.left(90)  
+    t1.fd(200)  
+    mytracks.append(t1.pos()) 
+    return mytracks 
+
+ 
+def replayTracks(mytracks): 
+	t1.penup()
+	t1.goto(-400,300)
+	t1.pendown()
+	t1.pencolor("Red")
+	for t in mytracks: 
+        	t1.goto(t) 
+
+ 
+def lab7_4(): 
+    mytracks=saveTracks() 
+    replayTracks(mytracks) 
+     
+def main(): 
+    lab7_4() 
+
+if __name__=="__main__":  
+     main()  
+ 
+wn.exitonclick() 
