b7d323f1d408d1ee3fc4f892271584e0be92f3fa
fud200 <fud1020@naver.com>
2016-05-09 12:57:27 +0900

    10주차 커피 비율과제
    10주차 커피 비율과제

---
+++ w10/w10main.py
@@ -0,0 +1,14 @@
+﻿alldata= [
+    ["Coffee","Water","Milk","Icecream"],
+    ["Espresso","No","No","No"],
+    ["Long Black","Yes","No","No"],
+    ["Flat White","No","Yes","No"],
+    ["Cappuccino","No","Yes","No"],
+    ["Affogatto","No","No","Yes"]
+]
+data= alldata[1:]
+sum=0
+for i in data:
+    if i[2].upper()=='YES':
+        sum=sum+1
+print "percent:",float(sum)/float(len(data))
