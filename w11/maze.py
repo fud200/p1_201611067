82ce9bfa564b8245af064b5e55b08ae8f8d102df
fud200 <fud1020@naver.com>
2016-05-16 12:20:08 +0900

    키보드미로게임
    키보드미로게임

---
+++ w11/maze.py
@@ -0,0 +1,22 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+wn.bgpic("mymaze.gif")
+def k1():
+    t1.fd(50)
+wn.onkey(k1,"Up")
+def k2():
+    t1.lt(90)
+def k3():
+    t1.right(90)
+def k4():
+    t1.bk(50)
+wn.onkey(k2,"Left")
+wn.onkey(k3,"Right")
+wn.onkey(k4,"Down")
+wn.listen()
+t1.penup()
+t1.goto(-300,350)
+
+
+wn.exitonclick()
