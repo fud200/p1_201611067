ea8a48f08563e434f6dbf5739b09ff67446504d3
fud200 <fud1020@naver.com>
2016-06-01 18:25:12 +0900

    14주차 강아지 짖는소리
    14주차 강아지 짖는소리

---
+++ w14/w14main.py
@@ -0,0 +1,21 @@
+﻿def lab14():
+    class Dog(object):
+        def talk(self):
+            print "Dog's barking sound is mung mung..."
+        
+    class ShihtzuDog(Dog):
+        def talk(self):
+            print "ShihtzuDog's barking sound is si si..."
+        
+    class Maltese(Dog):
+        def talk(self):
+            print "Maltese's barking sound is mal mal..."
+    mydog1=Dog()
+    mydog2=ShihtzuDog()
+    mydog3=Maltese()
+    mydog1.talk()
+    mydog2.talk()
+    mydog3.talk()
+    
+lab14()
+sel1=raw_input()
